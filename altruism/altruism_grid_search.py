2c83a2653e7f5878c24ce0474e1f84a19274160e
doesburg11 <petervandoesburg11@gmail.com>
2025-09-28 21:03:05 +0200

    Update harshness parameter values for grid search in altruism model

--- altruism/altruism_grid_search.py
+++ altruism/altruism_grid_search.py
@@ -38,7 +38,7 @@ def main():
         'benefit_from_altruism': [clamp(round(x, 2)) for x in np.arange(0.00, 1.00 + 0.01, 0.01)],
         'cost_of_altruism': [clamp(round(x, 2)) for x in np.arange(0.00, 0.35 + 0.01, 0.01)],
         'disease': [0.26],
-        'harshness': [0.0, 0.10, 0.20, 0.30, 0.40],
+        'harshness': [0.96, 0.97, 0.98, 0.99, 1.00],
         # [clamp(round(x, 2)) for x in np.arange(0.85, 0.95 + 0.01, 0.01)]  # 0.85, 0.86, ..., 0.89
     }
     param_names = list(grid.keys())
